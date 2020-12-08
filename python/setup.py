99679fe99f5f54716cf74f1b3a18131cb9659b6c
nolash <dev@holbrook.no>
2020-12-08 18:02:14 +0100

    Add missing package path in setup config

--- python/setup.py
+++ python/setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup
 
 setup(
+        package_data={
+            '': [
+                'data/GiftableToken.abi.json',
+                'data/GiftableToken.bin',
+                ],
+            },
+        include_package_data=True,
         )
