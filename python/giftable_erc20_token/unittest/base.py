637080c4641fc2cf0242e8f1f44d717c7bfd3336
lash <dev@holbrook.no>
2023-07-27 07:36:33 +0100

    Factor out token publish in giftable token unittest fixture

--- python/giftable_erc20_token/unittest/base.py
+++ python/giftable_erc20_token/unittest/base.py
@@ -22,25 +22,33 @@ class TestGiftableToken(EthTesterCase):
     def setUp(self):
         super(TestGiftableToken, self).setUp()
         self.conn = RPCConnection.connect(self.chain_spec, 'default')
+       
+        address = self.publish_giftable_token('Foo Token', 'FOO', 16, expire=self.expire)
+        self.address = to_checksum_address(address)
         nonce_oracle = RPCNonceOracle(self.accounts[0], conn=self.conn)
         c = GiftableToken(self.chain_spec, signer=self.signer, nonce_oracle=nonce_oracle)
-        self.symbol = 'FOO'
-        self.name = 'Foo Token'
-        self.decimals = 16
-        (tx_hash, o) = c.constructor(self.accounts[0], self.name, self.symbol, self.decimals, expire=self.expire)
-        self.rpc.do(o)
-        o = receipt(tx_hash)
-        r = self.rpc.do(o)
-        self.assertEqual(r['status'], 1)
-        self.address = to_checksum_address(r['contract_address'])
-        logg.debug('published on address {} with hash {}'.format(self.address, tx_hash))
-
         self.initial_supply = 1 << 40
         (tx_hash, o) = c.mint_to(self.address, self.accounts[0], self.accounts[0], self.initial_supply)
         r = self.conn.do(o)
         o = receipt(tx_hash)
         r = self.conn.do(o)
         self.assertEqual(r['status'], 1)
+
+
+    def publish_giftable_token(self, name, symbol, decimals=16, expire=None):
+        nonce_oracle = RPCNonceOracle(self.accounts[0], conn=self.conn)
+        c = GiftableToken(self.chain_spec, signer=self.signer, nonce_oracle=nonce_oracle)
+        self.symbol = name
+        self.name = symbol
+        self.decimals = decimals
+        (tx_hash, o) = c.constructor(self.accounts[0], self.name, self.symbol, self.decimals, expire=expire)
+        self.rpc.do(o)
+        o = receipt(tx_hash)
+        r = self.rpc.do(o)
+        self.assertEqual(r['status'], 1)
+        address = r['contract_address'] 
+        logg.debug('published on address {} with hash {}'.format(address, tx_hash))
+        return address
 
 
 class TestGiftableExpireToken(TestGiftableToken):
